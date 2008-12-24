aee0101d806ed6b2cc69e7c77c6a434b8999a6f0
Joe Groff <arcata@gmail.com>
2008-12-23 17:45:10 -0800

    make sure default settings take hold

--- main.m
+++ main.m
@@ -3,6 +3,14 @@
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    [[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
+        @"3", @"deathPulses",
+        @"1", @"arrows",
+        [NSNumber numberWithBool:YES], @"flowerShower",
+        nil
+    ]];
+
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
     return retVal;
